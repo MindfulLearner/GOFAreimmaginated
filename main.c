44689c200833d416896b1e66f47e6fb38bbbc157
MindfulLearner <dimajosh1009@gmail.com>
2024-07-13 15:15:29 +0000

    inizializzazione logica

--- main.c
+++ main.c
@@ -1,34 +1,78 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-
-    // Pianeti
-    typedef struct { 
-    
-        int Caccia;
-        int Bombardieri;
-        int Intercettatori;
-    } PianetaA;
-
-    // Giocatore e Computer
-    typedef struct 
-    
-        int CacciaGiocatore        
-
-
-    };
-    
+#include <stdio.h> #include <stdlib.h> #include <time.h>
 
 
 
+// Giocatore e Computer
+typedef struct { 
+	int CacciaGiocatore        
+} Giocatore;
+
+// Pianeti
+typedef struct { 
+	int CacciaGiocatore;
+} PianetaA;
 
 
+void moveNaviGiocatore (){
+	//SPOSTIAMO INT DI struct giocatore a struct pianeta 
+	CacciaGIocatore.GIocatore = > CacciaGiocatore.Giocatore 
+
+}
+
+
+void produciNaviGiocatore () {
+	int naviInProduzion;
+	prinft( inserisci quante navi vuoi produre)
+		scanf (metti numero da navi da produrre)
+		int naviInProduzion = naviInProduzion + scanf inserito sicuramente un int ) 
+	int naviPronte; 
+	printf (testo che chiede di scegli quante navi vuoi printare)
+		scanf (inseriamo quante navi vogliamo mettere )
+		int navipronto = navipronte + naviPronte 
+
+}
+
+// inizio programma
 int main () {
+	printf(testo scegli comando scrivi help se hai bisogno di capire cosa puoi fare  )
+		scanf ( scrivi cosa vuoi fare tr ale farie funzioni)
+
+		// LOGIC PRIMA DI INVIARE NAVI DI GIOCARTORE A PIANETA A BISOGNA PRIMA PRODURLE QUINDI INIUZIAMO PRIMA CON funzione prodcuinavigiocatore a n infinito di navi per ora, dopo di che procediamo all utilizzo di movenavigioctoire in cui spostermo i dati  int navi in produzione che saranno sicuramente puntatori perche cosi si possono cambiare tramite adresss e devono essere salvate in struct giocatore
+		// Logico ORA PRIMA DI INVIARE METTERE IF CHE CONTROLLA PRIMA CHE LE NAVI PRONTE SIANO NON UGUALE A 0 E CHE SIANO ANCHE DISPONIBILI AL NUMERO DI NAVI CHE ABBIAMO SCELTO DOPO DI CHE CON 
+		// CON move navi giocatore dobbiamo portare navi pronte in struct pianeti nel quale dovremmo sommare navi in pianeti e dopo di che sottrarre in struct giocatore 
+		// in generale la logica e 
+		// caricare 
+		// spostare 
+		// calcolo 
+		// fine
+
+
+		// piu che altro capire come metterle tra i vazri posti questo e solo un test per c
+
+		WHILE GIGATE {
+			// iniziamo con la prima chiamata 
+			if ( scanf = PRODUCINAVVI) 
+				produciNaviGiocatore();
+		}
+
+	// seconda chaimata 
+	if (scanf = moveNaviGiocatore) {
+		moveNaviGiocatore()
+	}
+}
+
+
+
+
+printf e stato scaricato tutto
 
 
 
 
 
-    return 0;
+
+
+
+
+return 0;
 }
