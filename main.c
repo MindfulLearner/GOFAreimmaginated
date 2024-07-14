c7aa966ba03d1cb859dbefec06243c1129464b0f
MindfulLearner <dimajosh1009@gmail.com>
2024-07-14 18:45:21 +0000

    logica sistemata

--- main.c
+++ main.c
@@ -4,84 +4,36 @@
 
 // Giocatore e Computer
 typedef struct { 
-	int CacciaGiocatore        
+	int giocatoreNaviCaccia 
 } Giocatore;
 
 // Pianeti
 typedef struct { 
-	int CacciaGiocatore;
+	int giocatoreNaviCaccia;
 } PianetaA;
 
+//inizializzazione Giocatore a 0
+int initGiocatore {
+	
 
-void moveNaviGiocatore (){
-	//SPOSTIAMO INT DI struct giocatore a struct pianeta
-
-
-	print ( inserire quante navi da mandare)
-		scanf (utente inseriesce quante navi mandare)
-
-		if naviInProduzionpuntatore ovviamente = 0 or minore di richiesta dellutent)
-		{
-		 prendiamo da struct, spostiamo da struct giocatore i dati di int caccia giocare a pianeti	
-		} else {
-printf (non ci sono abbastanza navi da mandare)''
-	return errore 
-		}
-	CacciaGIocatore.GIocatore = > CacciaGiocatore.Giocatore 
 
 }
 
 
-void produciNaviGiocatore () {
-	int naviInProduzion;
-	prinft( inserisci quante navi vuoi produre)
-		scanf (metti numero da navi da produrre)
-		int naviInProduzion = naviInProduzion + scanf inserito sicuramente un int ) 
-		int naviPronte; 
-	printf (testo che chiede di scegli quante navi vuoi printare)
-		scanf (inseriamo quante navi vogliamo mettere )
-		int navipronto = navipronte + naviInProduzion 
+void giocatoreMoveNavi (){
+	//SPOSTIAMO INT DI struct giocatore a struct pianeta
+}
 
-		// grande problema ovvero che rimane buffer dobbiamo spostare idati a struct giocatore e poi passarte clearre il buffer di questi cosi non azzeriamo il contatore per le voci di producinavi
-
+void giocatoreProduciNavi () {
+	// richiesta di produzione navi si controll si parte dal presssuposto che verra usata questa richiest 	
 }
 
 // inizio programma
 int main () {
-	printf(testo scegli comando scrivi help se hai bisogno di capire cosa puoi fare  )
-		scanf ( scrivi cosa vuoi fare tr ale farie funzioni)
 
-		// LOGIC PRIMA DI INVIARE NAVI DI GIOCARTORE A PIANETA A BISOGNA PRIMA PRODURLE QUINDI INIUZIAMO PRIMA CON funzione prodcuinavigiocatore a n infinito di navi per ora, dopo di che procediamo all utilizzo di movenavigioctoire in cui spostermo i dati  int navi in produzione che saranno sicuramente puntatori perche cosi si possono cambiare tramite adresss e devono essere salvate in struct giocatore
-		// Logico ORA PRIMA DI INVIARE METTERE IF CHE CONTROLLA PRIMA CHE LE NAVI PRONTE SIANO NON UGUALE A 0 E CHE SIANO ANCHE DISPONIBILI AL NUMERO DI NAVI CHE ABBIAMO SCELTO DOPO DI CHE CON 
-		// CON move navi giocatore dobbiamo portare navi pronte in struct pianeti nel quale dovremmo sommare navi in pianeti e dopo di che sottrarre in struct giocatore 
-		// in generale la logica e 
-		// caricare 
-		// spostare 
-		// calcolo 
-		// fine
+// configurazione init a 0 di struct Giocatori
 
-
-		// piu che altro capire come metterle tra i vazri posti questo e solo un test per c
-
-		WHILE GIGATE {
-			// iniziamo con la prima chiamata 
-			if ( scanf = PRODUCINAVVI) 
-				produciNaviGiocatore();
-		}
-
-	// seconda chaimata 
-	if (scanf = moveNaviGiocatore) {
-		moveNaviGiocatore()
-	}
-}
-
-
-
-
-printf e stato scaricato tutto
-
-
-
+	Giocatore josh 
 
 
 
