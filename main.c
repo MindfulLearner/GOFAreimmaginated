5bbe3d217a5db16d73196b206a7dd0f55b0fd86a
MindfulLearner <dimajosh1009@gmail.com>
2024-07-24 00:42:19 +0200

    produci navi init

--- main.c
+++ main.c
@@ -4,37 +4,44 @@
 #include <stdbool.h>
 #include <string.h>
 
+typedef struct {
+    int cacciaPlayer;
+} giocatore;
+typedef struct {
+    int cacciaPlayer;
+} pianeta;
 
 
-// // Giocatore e Computer
-// typedef struct { 
-// 	int giocatoreNaviCaccia 
-// } Giocatore;
-// 
-// // Pianeti
-// typedef struct { 
-// 	int giocatoreNaviCaccia;
-// } PianetaA;
-// 
-// //inizializzazione Giocatore a 0
-// int initGiocatore {
-// }
 
 
-// 	//SPOSTIAMO INT DI struct giocatore a struct pianeta
-// void giocatoreMoveNavi (){
-// }
-// 
+// STRUTTURA PLAYER
+giocatore player = {0};
 
+
+
+// BOOL
+bool whileComandi = true;
+
+
+
+
+// produci navi da mettere poi limite
+void produciNavi(giocatore *giocatore) {
+    int nuoveNavi;
+    printf("Quanti navi vuoi produrre?\n");
+    scanf("%d", &nuoveNavi);
+    giocatore->cacciaPlayer += nuoveNavi;
+}
 
 const char *arrayComando[] = {
     "HELP",
     "PRODUCINAVI",
     "MANDANAVI",
-    "EXIT"
+    "EXIT",
+        "USCITATOTALE"
 }; 
 
-char* richiestaComandi() {
+char* bufferComandi() {
     bool booleanoComandi = true;
     static char comando[50];
     while (booleanoComandi) {
@@ -45,15 +52,55 @@ char* richiestaComandi() {
                 return comando;
             }
         }
-S COMMANDI GIOCO -----------------------------------
-    // ----------------------------------------------------------------------------------------------
+        printf("Comando Errato riprova\n");
+    }
+}
 
-    while (true) { 
-        char *inputUtente = richiestaComandi(); 
-        printf("%s\n", inputUtente);
-        risposteComandi(inputUtente);
+bool risposteComandi(const char* inputUtente) {
+    if (strcmp(inputUtente, arrayComando[0]) == 0) {
+        printf("-------------------------------------------\n");
+        printf("I comandi che hai a disposizione sono:\n");
+        printf("'TUTORIAL' per vedere le istruzioni di gioco e come gicoare\n");
+        printf("'PRODUCINAVI' per produrre navi\n");
+        printf("'MANDANAVI' per mandare le navi ai pianeti\n");
+        printf("'EXIT' per andare in Lobby Main\n");
+        printf("'USCITATOTALE' per uscire totalemente\n");
+        printf("-------------------------------------------\n");
+    } else if (strcmp(inputUtente, arrayComando[1]) == 0) {
+        printf("-------------------------------------------\n");
+        printf("PRODUCINAVISWAG\n");
+        produciNavi(&player);
+        printf("-------------------------------------------\n");
+    } else if (strcmp(inputUtente, arrayComando[2]) == 0) {
+        printf("-------------------------------------------\n");
+        printf("MANDANAVI\n");
+        printf("-------------------------------------------\n");
+    } else if (strcmp(inputUtente, arrayComando[3]) == 0) {
+        printf("RITORNO A MAIN\n");
+        return whileComandi = false;
+    }
+     else if (strcmp(inputUtente, arrayComando[4]) == 0) {
+        printf("FINE PROGRAMMA..");
+        exit(1);
     }
 
+}
+
+// inizio programma
+int main () {
+
+
+    // configurazione init a 0 di struct Giocatori
+    // configuro la prompt comandi 
+    // ----------------------------------------------------------------------------------------------
+    // ------------------------------------PROMPTS COMMANDI GIOCO -----------------------------------
+    // ----------------------------------------------------------------------------------------------
+    
+    while (whileComandi == true) { 
+        char *inputUtente = bufferComandi(); 
+        risposteComandi(inputUtente);
+    }
+    printf("Totalenavi: %d\n", player.cacciaPlayer);
     return 0;
 }
 
